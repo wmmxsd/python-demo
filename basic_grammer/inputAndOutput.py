f3cde510919ff582579f819aaed1a4a319d1f8b4
wangmingming <wangmingming1@vrvmail.com.cn>
2024-09-02 18:16:42 +0800

    update

---
+++ basic_grammer/inputAndOutput.py
@@ -0,0 +1,19 @@
+200 + 300
+print (200 + 300)
+print ('###########')
+a = 100
+if a >= 101:
+    print(a)
+elif a == 100:
+    print(a + 1)
+    print(a + 2)
+else:
+    print(a + 3)
+print ('###########')
+name = input('请输入姓名：')
+sex = input('请输入性别：')
+while sex != "男" and sex != '女':
+    print('性别输入错误，请重新输入')
+    sex = input('请输入性别：')
+age = input('请输入年纪：')
+print(name, '：', sex, '，', age, '岁')
---
+++ basic_grammer/stringAndEncode.py
@@ -0,0 +1,37 @@
+#在最新的Python 3版本中，字符串是以Unicode编码的，也就是说，Python的字符串支持多语言。
+#Unicode编码用2个字节代表1个字符，支持所有语言比如中文、日文、韩文、英语等等。
+print ('###########')
+print ('中文English')
+print ('###########')
+#ord()方法获取字符的数字表示
+print ('###########')
+print ('ord(\'A\'):', ord('A'))
+print ('###########')
+#chr()将数字转为字符串
+print ('###########')
+print ('chr(65):', chr(65))
+print ('###########')
+#字符串和字节相互转换
+print ('###########')
+print('ABC'.encode('ascii'))
+print('中文'.encode('utf-8'))
+print(b'ABC'.decode('ascii'))
+print(b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8'))
+print ('###########')
+#len()获取字符串长度
+print ('###########')
+print('SONY无线耳机字符长度：', len('SONY无线耳机'))
+print ('###########')
+print ('###########')
+print ('###########')
+print ('###########')
+print ('###########')
+print ('###########')
+print ('###########')
+print ('###########')
+print ('###########')
+print ('###########')
+print ('###########')
+print ('###########')
+print ('###########')
+print ('###########')
