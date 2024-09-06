1d381fb3fa982e43fa2454ec1578f2e618767eb4
wangmingming <wangmingming1@vrvmail.com.cn>
2024-09-06 18:16:56 +0800

    update

--- basic_grammer/advanced_features.py
+++ basic_grammer/advanced_features.py
@@ -2,6 +2,7 @@
 高级特性
 """
 from collections.abc import Iterable
+import os
 
 print("###### 切片 ######")
 print("###### 列表切片 ######")
@@ -90,3 +91,41 @@ elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
     print('测试失败4!')
 else:
     print('测试成功!')
+
+print("###### 列表生成式 ######")
+# 利用[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]生成[1x1, 2x2, 3x3, ..., 10x10]
+# 方法1
+L = []
+print(L)
+for x in list(range(1, 11)):
+    L.append(x * x)
+print(L)
+# 方法2（列表生成式，更简洁）
+L = [x * x for x in list(range(1, 11))]
+print(L)
+# 列表生成式后面可以接if判断
+L = [x * x for x in list(range(1, 11)) if x % 2 == 0]
+print(L)
+L1 = ['Hello', 'World', 18, 'Apple', None]
+L2 = [x for x in L1 if isinstance(x, str)]
+print(L2)
+# 列表生成式for前面可以接if else表达式
+L = [x * x  if x % 2 == 0 else -x for x in list(range(1, 11))]
+print(L)
+# 列表生成式支持多层循环
+L = [m + '-' + n for m in 'ABC' for n in '123']
+print(L)
+L = [m + '-' + n + '-' + o for m in 'ABC' for n in '123' for o in ('0', '1', '1')]
+print(L)
+L = ['目录/文件：' + d for d in os.listdir('/')]
+print(L)
+# 列表元素为二元组
+L = [(source, dest) for source in list(range(1, 3)) for dest in list(range(11, 13))]
+print(L)
+# 列表生成式的可以使用多个变量
+L = [{k, v} for k, v in {1: 'A', 2: 'B'}.items()]
+print(L)
+
+print("###### 生成器 ######")
+# 通过列表生成式，我们可以直接创建一个列表。但是，受到内存限制，列表容量肯定是有限的。而且，创建一个包含100万个元素的列表，不仅占用很大的存储空间，如果我们仅仅需要访问前面几个元素，那后面绝大多数元素占用的空间都白白浪费了。
+# 所以，如果列表元素可以按照某种算法推算出来，那我们是否可以在循环的过程中不断推算出后续的元素呢？这样就不必创建完整的list，从而节省大量的空间。在Python中，这种一边循环一边计算的机制，称为生成器：generator。
